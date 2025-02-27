1efe9a28da74a45c4a0da9cbfa8c26a49e962316
Emfox Zhou <emfoxzhou@gmail.com>
2025-02-27 04:34:34 -0500

    Fixes to build on old debian(squeeze).

--- image/utilities.sh
+++ image/utilities.sh
@@ -4,7 +4,7 @@ source /bd_build/buildconfig
 set -x
 
 ## Often used tools.
-$minimal_apt_get_install curl less vim-tiny psmisc gpg-agent dirmngr
+$minimal_apt_get_install curl less vim-tiny psmisc gnupg-agent dirmngr
 ln -s /usr/bin/vim.tiny /usr/bin/vim
 
 ## This tool runs a command as another user and sets $HOME.
@@ -12,3 +12,10 @@ cp /bd_build/bin/setuser /sbin/setuser
 
 ## This tool allows installation of apt packages with automatic cache cleanup.
 cp /bd_build/bin/install_clean /sbin/install_clean
+
+## argparse not available on python3.1 of Debian squeeze
+$minimal_apt_get_install python3 python3-setuptools
+mkdir argparse
+curl -s https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/argparse/argparse-1.2.1.tar.gz | tar -xzv -C argparse --strip-components=1
+cd argparse
+python3 setup.py install
